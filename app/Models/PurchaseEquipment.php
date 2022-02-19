86cc18242a3393339c59642211a3bfa8515a2699
Adi <adi.rws@gmail.com>
2022-02-19 14:00:37 +0700

    add codes

--- app/Models/PurchaseEquipment.php
+++ app/Models/PurchaseEquipment.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Carbon\Carbon;
 
 class PurchaseEquipment extends Model implements HasMedia
 {
@@ -91,5 +92,13 @@ class PurchaseEquipment extends Model implements HasMedia
     protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format('Y-m-d H:i:s');
+    }
+
+    public static function boot() {
+        parent::boot();
+        static::creating(function($model) {
+            $model->number = PurchaseEquipment::whereYear('created_at', Carbon::now()->year)->where('team_id', $model->team_id)->max('number') + 1; // start from 1 every year
+            $model->code = 'PEM' . str_pad($model->number, 4, '0', STR_PAD_LEFT);
+        });
     }
 }
